7f9f6fa4204f79857c98deeb04ac3daa8dec83a9
TuNombre <jhonierstiven1807@gmail.com>
2026-03-01 23:12:24 -0500

    feat: Implementacion de los requirimientos tal cual descritos RFADMIN 11 Y 12, correspondientes a listar y cambiar estado de medicametos, -Creacion de la vista pricipal listar_medicamentos en products/views.py. API JSON api_listar_medicamentos pensada para que el frontend pueda actualizar el listado en tiempo real. Exportacion de excel y pdf. Implementación del uso de la s nuevas rutas archivo urls.py en products. Creacion del template. Se agrega la seccion en el archivo templates/base. Implementacion del requerimiento cambio de estado, nueva accion en de CAMBIO DE ESTADO, propiedad de 'puede venderse', creacion de la vista cambiar estado, actualizacion de rutas, nueva columna en el template listamed, se incluye propiedad en la api para que eel modulo excluya medicamentos no disponibles

--- Curatio/products/urls.py
+++ Curatio/products/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path("medicamentos/crear/", views.crear_medicamento, name="crear_medicamento"),
+    path("medicamentos/", views.listar_medicamentos, name="listar_medicamentos"),
+    #URL para cambiar estado de medicamento solo para ADMIN
+    path("medicamentos/<int:pk>/cambiar-estado/", views.cambiar_estado_medicamento, name="cambiar_estado_medicamento"),
+    #URL para listar medicamentos
+    path("api/medicamentos/", views.api_listar_medicamentos, name="api_listar_medicamentos"),
+    #URL para reporte de medicamentos
+    path("medicamentos/reporte/", views.reporte_medicamentos, name="reporte_medicamentos"),
+    #URL para presentaciones por forma
     path("api/presentaciones/", views.presentaciones_por_forma, name="presentaciones_por_forma"),
 ]
